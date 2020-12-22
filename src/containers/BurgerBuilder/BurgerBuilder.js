25aafc0bdc161acf3c5775547524dc8a6cb73966
Ashish Kumar <askumaryg@gmail.com>
2020-12-22 11:47:13 +0530

    Testing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import axios from '../../axios-order';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import * as actions from '../../store/actions/index';
 
-class BurgerBuilder extends Component{
+export class BurgerBuilder extends Component{
     // constructor(props) {
     //     super(props);
     //     this.state = {...}
